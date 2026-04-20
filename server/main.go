9227f01dd2524300420271bbdbbd2c6ef07c9bd5
Andrei Gorneanu <Andrei.Gorneanu@riverbed.com>
2026-04-20 20:53:40 +0300

    Enable mTLS(lab 7)

--- server/main.go
+++ server/main.go
@@ -8,6 +8,8 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+	"crypto/tls"
+	"crypto/x509"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/otiai10/gosseract/v2"
@@ -20,6 +22,29 @@ import (
 
 // TODO: Implement mTLS security
 // See docs/SECURITY_IMPLEMENTATION.md for instructions on how to configure TLS
+func NewTLSConfig() *tls.Config {
+    // Încărcarea certificatului CA
+    certpool := x509.NewCertPool()
+    pemCerts, err := os.ReadFile("/run/secrets/ca.crt")
+    if err != nil {
+        panic(err)
+    }
+    certpool.AppendCertsFromPEM(pemCerts)
+ 
+    // Încărcarea certificatului de client
+    cert, err := tls.LoadX509KeyPair("/run/secrets/web.crt", "/run/secrets/web.key")
+    if err != nil {
+        panic(err)
+    }
+ 
+    return &tls.Config{
+        RootCAs:            certpool,
+        ClientCAs:          certpool,
+        Certificates:       []tls.Certificate{cert},
+        InsecureSkipVerify: false,
+    }
+}
+
 
 func main() {
 	// Connect to MongoDB
@@ -50,9 +75,11 @@ func main() {
 	defer ocrClient.Close()
 	brokerHandler := broker.NewBrokerHandler(db, ocrClient)
 
+	tlsconfig := NewTLSConfig()
+
 	opts := mqtt.NewClientOptions()
-	opts.AddBroker("tcp://broker:1883")
-	opts.SetClientID("web")
+	opts.AddBroker("ssl://broker:8883")
+	opts.SetClientID("web").SetTLSConfig(tlsconfig)
 
 	// Start the connection
 	client := mqtt.NewClient(opts)
